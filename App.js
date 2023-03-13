796657ecba1bef5ae28159e572e24f8fc0937648
dhrumit <dhrumitbarot1@gmail.com>
2023-03-13 16:04:54 -0600

    remove code from copied app.js and redesigning it. start's with changeng name of const.

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import {React, Node} from 'react';
 import {
   SafeAreaView,
@@ -17,17 +9,10 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
-const Section = ({children, title}) => {
+const App = () => {
+  
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -75,6 +60,10 @@ const App = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
          <Text>DEMO</Text>
+         <Todo
+         key='6'
+         todo='some text'
+         />
         </View>
       </ScrollView>
     </SafeAreaView>
