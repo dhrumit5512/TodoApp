0a09aabead4a480ba47c2250c860120ddbf6218a
dhrumit <dhrumitbarot1@gmail.com>
2023-03-13 19:29:02 -0600

    there is error in name of styling and viewing tag, that has to be Item instead of Text. took too long to solve.

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ const App = () => {
 
   return (
     <View style={styles.todo}>
-      <Text
+      <Item
           key={item.id}
           item={item}
           onPress={() => completedItem(item.id)}
